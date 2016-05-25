b790f4ddd87bd5e718f587f580ae657c3c46ac3d
keti <k.zaridze@gmail.com>
2016-05-25 19:16:38 +0200

    Update README - project description and installation

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# robokind_r25_ros_wrapper
+Robokind R25 ROS Wrapper
+=================
+
+ROS Wrapper publishes and subscribes messages to manipulate Robokind R25 robot from the application. Messages include information about robot position(joint) values, speaker text, robot state and sensor data. Wrapper communicates to robot using Robokind Java API.
+
+### Pre-requisites
+
+Supported operating system: Ubuntu (currently ros only works with Linux)
+
+ROS: You need to install [RosJava](http://wiki.ros.org/rosjava/Tutorials/indigo/Installation).
+
+
+### Installation
+
+- Create RosJava catkin package manually or run the following command: 
+
+    ```
+    catkin_create_rosjava_pkg PACKAGE_NAME
+    ``` 
+
+
+
+- Create RosJava catkin gradle subproject by running the following command: 
+
+    ```
+    catkin_create_rosjava_project PROJECT_NAME
+    ``` 
+
+- Add project files under your subproject directory and add Robokind API dependencies in build.gradle file: 
+
+    ```
+    dependencies {
+       compile 'org.robokind:org.robokind.client.basic:0.9.5'
+       compile 'org.robokind:org.robokind.api.motion:0.9.5'
+       compile 'org.robokind:org.robokind.api.messaging:0.9.5'
+    }
+    ```
+
+- Build project's custom messages using the following commands:
+
+    ```
+    source ~/rosjava/devel/setup.bash
+    genjava_message_artifacts --verbose -p std_msgs rosjava_r25_msgs
+    ```
+
+- Add newly built custom messages under build.gradle dependencies:
+    ```
+    compile 'org.ros.rosjava_messages:rosjava_r25_msgs:0.2.1'
+    ```
+
+- Run AppNode and BridgeNode files after launching roscore 
--- Robokind/InteractionDemo.java
+++ Robokind/InteractionDemo.java
@@ -1,4 +1,5 @@
 package com.github.rosjava.my_pub_sub_tutorial.Robokind;
+
 import org.robokind.api.motion.messaging.RemoteRobot;
 import org.robokind.api.speech.messaging.RemoteSpeechServiceClient;
 
@@ -15,95 +16,96 @@ import org.robokind.api.motion.Robot.RobotPositionHashMap;
 import org.robokind.client.basic.Robokind;
 
 public class InteractionDemo {
-    // class variables
-    private static RobotPositionMap myGoalPositions;
-    private static RemoteRobot myRobot;
-    private static int timeFrame;
-    private static RemoteSpeechServiceClient mySpeaker;
-    public static void main(String[] args) throws InterruptedException {
-    	String robotID = "myRobot";
-        String robotIP = "192.168.2.3";
-        // set addresses
-        SetSettings settings = new SetSettings(robotID, robotIP);
-        // make connection
-        myRobot = Robokind.connectRobot();
-        mySpeaker = Robokind.connectSpeechService();
-        // check if robot is connected
-        if (myRobot.isConnected()) {
-        	
-        	Expression expression = new Expression();
-        	expression.setExpressionJoints(myRobot);
-        	
-        	Letters letters = new Letters();
-        	
-//    		mySpeaker.speak("Hi! Would you like to play a Letter game?");
-//    		Robokind.sleep(900);
-//    		expression.smile(700, myRobot);
-//    		//expression.blink(500, myRobot);
-//    		Robokind.sleep(400);
-//    		mySpeaker.speak("I will point you to the rectangels and squares placed on the table and you have to construct letters from them");
-//    		Robokind.sleep(3000);
-//    		mySpeaker.speak("Lets start the game!");
-//    		Robokind.sleep(800);
-//        	letters.describeLetter("T", 12000, mySpeaker, myRobot);
+	// class variables
+	private static RobotPositionMap myGoalPositions;
+	private static RemoteRobot myRobot;
+	private static int timeFrame;
+	private static RemoteSpeechServiceClient mySpeaker;
 
-            double default_neck_pitch_position = 0.4;
-            double left_shoulder_pitch_min = 0.185;
-            double left_shoulder_pitch_max = 1;
-            
-            // object position with respect to the robot
-            double x = 20;
-            double z = 30;
-            double y = 50;
-            
-            double d = Math.sqrt(Math.pow(x,2) + Math.pow(z,2)); 
-            
-            JointId neck_pitch_Id = new JointId(myRobot.getRobotId(), new Joint.Id(200));
-            JointId neck_yaw_Id = new JointId(myRobot.getRobotId(), new Joint.Id(202));
-            JointId brows_Id = new JointId(myRobot.getRobotId(), new Joint.Id(300));
-            JointId left_elbow_yaw_Id = new JointId(myRobot.getRobotId(), new Joint.Id(410));
-            JointId left_elbow_pitch_Id = new JointId(myRobot.getRobotId(), new Joint.Id(411));
-            JointId left_shoulder_pitch_Id = new JointId(myRobot.getRobotId(), new Joint.Id(400));
-            JointId left_shoulder_roll_Id = new JointId(myRobot.getRobotId(), new Joint.Id(401));
-            JointId left_wrist_yaw_Id = new JointId(myRobot.getRobotId(), new Joint.Id(420));
-            JointId left_hand_grasp_Id = new JointId(myRobot.getRobotId(), new Joint.Id(421));
+	public static void main(String[] args) throws InterruptedException {
+		String robotID = "myRobot";
+		String robotIP = "192.168.2.3";
+		// set addresses
+		SetSettings settings = new SetSettings(robotID, robotIP);
+		// make connection
+		myRobot = Robokind.connectRobot();
+		mySpeaker = Robokind.connectSpeechService();
+		// check if robot is connected
+		if (myRobot.isConnected()) {
 
-            
-            double angle_v = Math.toDegrees(Math.atan(d/y));
-            double angle_h = Math.toDegrees(Math.atan(x/z));
-            
-            System.out.println(angle_h);
-            
-            double brows_value =  angle_v / 180;
-            double neck_pitch_value = default_neck_pitch_position * angle_v / 90;
-            double neck_yaw_value = (90 + angle_h) / 180;
-            
-            System.out.println(neck_yaw_value);
-            Robokind.sleep(400);
-            myGoalPositions = new RobotPositionHashMap();
-		    myGoalPositions.put(neck_pitch_Id, new NormalizedDouble(neck_yaw_value));
-            myGoalPositions.put(neck_yaw_Id, new NormalizedDouble(neck_pitch_value));;
-		    myGoalPositions.put(brows_Id, new NormalizedDouble(brows_value));
-		    myRobot.move(myGoalPositions, 400);
+			Expression expression = new Expression();
+			expression.setExpressionJoints(myRobot);
 
-		   
-		    myGoalPositions.put(left_elbow_yaw_Id, new NormalizedDouble(0.3));
-		    myGoalPositions.put(left_shoulder_pitch_Id, new NormalizedDouble(0.8));
-		    
-		    myGoalPositions.put(left_wrist_yaw_Id, new NormalizedDouble(0));
-		    myRobot.move(myGoalPositions, 400);
-		    Robokind.sleep(800);
-		    
-//		    myGoalPositions = myRobot.getDefaultPositions();
-//          myRobot.move(myGoalPositions, 1000);
+			Letters letters = new Letters();
 
-        	
-            ///////////////////////////////////////////
-            /// DISCONNECT AND EXIT
-            ///////////////////////////////////////////
-            
-            Robokind.disconnect();
-            System.exit(0);
-        }
-    }  
+			// mySpeaker.speak("Hi! Would you like to play a Letter game?");
+			// Robokind.sleep(900);
+			// expression.smile(700, myRobot);
+			// //expression.blink(500, myRobot);
+			// Robokind.sleep(400);
+			// mySpeaker.speak("I will point you to the rectangels and squares
+			// placed on the table and you have to construct letters from
+			// them");
+			// Robokind.sleep(3000);
+			// mySpeaker.speak("Lets start the game!");
+			// Robokind.sleep(800);
+			// letters.describeLetter("T", 12000, mySpeaker, myRobot);
+
+			double default_neck_pitch_position = 0.4;
+			double left_shoulder_pitch_min = 0.185;
+			double left_shoulder_pitch_max = 1;
+
+			// object position with respect to the robot
+			double x = 20;
+			double z = 30;
+			double y = 50;
+
+			double d = Math.sqrt(Math.pow(x, 2) + Math.pow(z, 2));
+
+			JointId neck_pitch_Id = new JointId(myRobot.getRobotId(), new Joint.Id(200));
+			JointId neck_yaw_Id = new JointId(myRobot.getRobotId(), new Joint.Id(202));
+			JointId brows_Id = new JointId(myRobot.getRobotId(), new Joint.Id(300));
+			JointId left_elbow_yaw_Id = new JointId(myRobot.getRobotId(), new Joint.Id(410));
+			JointId left_elbow_pitch_Id = new JointId(myRobot.getRobotId(), new Joint.Id(411));
+			JointId left_shoulder_pitch_Id = new JointId(myRobot.getRobotId(), new Joint.Id(400));
+			JointId left_shoulder_roll_Id = new JointId(myRobot.getRobotId(), new Joint.Id(401));
+			JointId left_wrist_yaw_Id = new JointId(myRobot.getRobotId(), new Joint.Id(420));
+			JointId left_hand_grasp_Id = new JointId(myRobot.getRobotId(), new Joint.Id(421));
+
+			double angle_v = Math.toDegrees(Math.atan(d / y));
+			double angle_h = Math.toDegrees(Math.atan(x / z));
+
+			System.out.println(angle_h);
+
+			double brows_value = angle_v / 180;
+			double neck_pitch_value = default_neck_pitch_position * angle_v / 90;
+			double neck_yaw_value = (90 + angle_h) / 180;
+
+			System.out.println(neck_yaw_value);
+			Robokind.sleep(400);
+			myGoalPositions = new RobotPositionHashMap();
+			myGoalPositions.put(neck_pitch_Id, new NormalizedDouble(neck_yaw_value));
+			myGoalPositions.put(neck_yaw_Id, new NormalizedDouble(neck_pitch_value));
+			;
+			myGoalPositions.put(brows_Id, new NormalizedDouble(brows_value));
+			myRobot.move(myGoalPositions, 400);
+
+			myGoalPositions.put(left_elbow_yaw_Id, new NormalizedDouble(0.3));
+			myGoalPositions.put(left_shoulder_pitch_Id, new NormalizedDouble(0.8));
+
+			myGoalPositions.put(left_wrist_yaw_Id, new NormalizedDouble(0));
+			myRobot.move(myGoalPositions, 400);
+			Robokind.sleep(800);
+
+			// myGoalPositions = myRobot.getDefaultPositions();
+			// myRobot.move(myGoalPositions, 1000);
+
+			///////////////////////////////////////////
+			/// DISCONNECT AND EXIT
+			///////////////////////////////////////////
+
+			Robokind.disconnect();
+			System.exit(0);
+		}
+	}
 }
